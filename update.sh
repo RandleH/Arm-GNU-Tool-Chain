05861edaa354b2731033128f1866933bfe639fb5
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2024-12-24 16:23:18 -0800

    Add update bash script

---
+++ update.sh
@@ -0,0 +1,9 @@
+git submodule status
+
+git submodule update --init macOS-Intel
+git submodule update --init macOS-AppleSilicon
+
+git submodule update --remote macOS-Intel
+git submodule update --remote macOS-AppleSilicon
+
+git status
